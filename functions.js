e4fa0945d4dcb42c7d6b60db8332aaaa0e04d8f6
Abdul Moiz <120182920+MoizKhan1179@users.noreply.github.com>
2022-12-28 17:58:18 +0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,54 @@
+// *********addFunction*******
+
+// function add(a,b){
+
+//     let result = Number(a) + Number(b)
+    
+//     return result
+//     // console.log(result)
+
+    
+// }
+// document.write(add(3,7))
+// console.log(add(3,7))
+// add(50,68)
+// add( prompt("Enter your First Data : "), prompt("Enter your Second Data : "))
+
+
+// ************ES6 function or Arrow Functions*************
+
+// let add = (a,b) => {
+//     let result = Number(a) + Number(b)
+//     console.log(result)
+// }
+
+// let c = prompt("Enter your First Data : ");
+// let d = prompt("Enter your Second Data : ");
+
+// let result = Number(c) + Number(d)
+// console.log(result)
+
+
+// add(c,d)
+
+
+
+
+// **************Square Function*************
+
+// let square = num => console.log(num**2)
+
+
+// square(7)
+
+
+
+// 
+
+let arrays =  [25,26,25,26,6,9,4]
+
+
+
+arrays.map((values)=>{
+    console.log(values)
+})
